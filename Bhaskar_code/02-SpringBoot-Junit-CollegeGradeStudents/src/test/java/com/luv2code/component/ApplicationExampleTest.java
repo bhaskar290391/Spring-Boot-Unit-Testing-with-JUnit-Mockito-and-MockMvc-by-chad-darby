d4bd8853019ced252758158c1b904ab15d366140
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-10 18:11:06 +0530

    adding properties and autowired bean in test class

--- Bhaskar_code/02-SpringBoot-Junit-CollegeGradeStudents/src/test/java/com/luv2code/component/ApplicationExampleTest.java
+++ Bhaskar_code/02-SpringBoot-Junit-CollegeGradeStudents/src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -1,15 +1,57 @@
 package com.luv2code.component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class ApplicationExampleTest {
 
+	private static int counter = 0;
+
+	@Value("${info.school.name}")
+	private String schoolName;
+
+	@Value("${info.app.name}")
+	private String appName;
+
+	@Value("${info.app.description}")
+	private String appDescription;
+
+	@Value("${info.app.version}")
+	private String appVersion;
+
+	@Autowired
+	private CollegeStudent collegeStudents;
+
+	@Autowired
+	private StudentGrades studentsGrades;
+
+	@BeforeEach
+	public void beforeEachTest() {
+		counter++;
+		System.out.println("Testing : " + appName + " which is " + appDescription + " version :" + appVersion);
+	
+		collegeStudents.setFirstname("Bhaskar");
+		collegeStudents.setLastname("Mudaliyar");
+		collegeStudents.setEmailAddress("mudaliyarbhaskar29@gmail.com");
+		
+		studentsGrades.setMathGradeResults(new ArrayList<>(List.of(98.0, 76.0, 55.75, 87.0)));
+		collegeStudents.setStudentGrades(studentsGrades);
+	
+	}
+
 	@Test
 	public void basciTest() {
-		
+
 	}
 }
