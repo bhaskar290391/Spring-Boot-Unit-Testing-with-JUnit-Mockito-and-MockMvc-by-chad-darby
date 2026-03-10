72d59fe50153cfff883925459ba2cc9aa97b246c
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-10 17:48:47 +0530

    adding skeleton for Spring boot unit testing

---
+++ Bhaskar_code/02-SpringBoot-Junit-CollegeGradeStudents/src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -0,0 +1,15 @@
+package com.luv2code.component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+
+	@Test
+	public void basciTest() {
+		
+	}
+}
