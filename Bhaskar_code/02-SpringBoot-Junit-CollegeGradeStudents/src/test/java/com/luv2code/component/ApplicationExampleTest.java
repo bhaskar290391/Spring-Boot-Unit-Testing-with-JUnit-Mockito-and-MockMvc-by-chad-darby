91835740eb8d7b173f4da288b657948ccd270100
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-10 18:19:26 +0530

    adding test for equals and not equals for adding student gardes

--- Bhaskar_code/02-SpringBoot-Junit-CollegeGradeStudents/src/test/java/com/luv2code/component/ApplicationExampleTest.java
+++ Bhaskar_code/02-SpringBoot-Junit-CollegeGradeStudents/src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -1,11 +1,12 @@
 package com.luv2code.component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,18 +41,30 @@ public class ApplicationExampleTest {
 	public void beforeEachTest() {
 		counter++;
 		System.out.println("Testing : " + appName + " which is " + appDescription + " version :" + appVersion);
-	
+
 		collegeStudents.setFirstname("Bhaskar");
 		collegeStudents.setLastname("Mudaliyar");
 		collegeStudents.setEmailAddress("mudaliyarbhaskar29@gmail.com");
-		
+
 		studentsGrades.setMathGradeResults(new ArrayList<>(List.of(98.0, 76.0, 55.75, 87.0)));
 		collegeStudents.setStudentGrades(studentsGrades);
-	
+
 	}
 
+	@DisplayName("Add student Grade Result Test equals")
 	@Test
-	public void basciTest() {
+	public void addStudentGradeResultTestEquals() {
 
+		assertEquals(316.75,
+				studentsGrades.addGradeResultsForSingleClass(collegeStudents.getStudentGrades().getMathGradeResults()));
+	}
+	
+	
+	@DisplayName("Add student Grade Result Test not equals")
+	@Test
+	public void addStudentGradeResultTestNotEquals() {
+
+		assertNotEquals(100,
+				studentsGrades.addGradeResultsForSingleClass(collegeStudents.getStudentGrades().getMathGradeResults()));
 	}
 }
