7d7acdbe32a4b05ff543a744368170e4e22277e4
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-11 14:12:09 +0530

    adding mockito to spring boot project

---
+++ Bhaskar_code/03-Springboot-mockito/src/test/java/com/luv2code/test/MockitoExampleTest.java
@@ -0,0 +1,63 @@
+package com.luv2code.test;
+
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.dao.ApplicationDao;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class MockitoExampleTest {
+
+	@Mock
+	private ApplicationDao repo;
+
+	@InjectMocks
+	private ApplicationService service;
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
+	private CollegeStudent students;
+
+	@Autowired
+	private StudentGrades grades;
+	
+	
+	@BeforeEach
+	public void beforeEachData() {
+		students.setFirstname("Bhaskar");
+		students.setLastname("Mudaliyar");
+		students.setEmailAddress("abc@test.com");
+		students.setStudentGrades(grades);
+	}
+	
+	@DisplayName("When and Verify")
+	@Test
+	public void checkWhenAndVerify() {
+		
+		when(repo.addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults())).thenReturn(100.0);
+		
+		assertEquals(100.0, service.addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults()));
+		
+		verify(repo,times(1)).addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults());
+		
+		
+	}
+}
