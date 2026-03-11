c5508c7ea66cbf0ddf3d8b89aca501fb5adcc4f4
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-11 14:36:04 +0530

    @MockitoBean

--- Bhaskar_code/03-Springboot-mockito/src/test/java/com/luv2code/test/MockitoExampleTest.java
+++ Bhaskar_code/03-Springboot-mockito/src/test/java/com/luv2code/test/MockitoExampleTest.java
@@ -1,6 +1,6 @@
 package com.luv2code.test;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -8,12 +8,11 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.luv2code.component.MvcTestingExampleApplication;
 import com.luv2code.component.dao.ApplicationDao;
@@ -24,10 +23,17 @@ import com.luv2code.component.service.ApplicationService;
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class MockitoExampleTest {
 
-	@Mock
+//	@Mock
+//	private ApplicationDao repo;
+//
+//	@InjectMocks
+//	private ApplicationService service;
+	
+
+	@MockitoBean
 	private ApplicationDao repo;
 
-	@InjectMocks
+	@Autowired
 	private ApplicationService service;
 
 	@Autowired
@@ -38,8 +44,7 @@ public class MockitoExampleTest {
 
 	@Autowired
 	private StudentGrades grades;
-	
-	
+
 	@BeforeEach
 	public void beforeEachData() {
 		students.setFirstname("Bhaskar");
@@ -47,17 +52,25 @@ public class MockitoExampleTest {
 		students.setEmailAddress("abc@test.com");
 		students.setStudentGrades(grades);
 	}
-	
+
 	@DisplayName("When and Verify")
 	@Test
 	public void checkWhenAndVerify() {
-		
+
 		when(repo.addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults())).thenReturn(100.0);
-		
+
 		assertEquals(100.0, service.addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults()));
+
+		verify(repo, times(1)).addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults());
+
+	}
+	
+	
+	@DisplayName("Find GPA")
+	@Test
+	public void findGPA() {
+		when(repo.findGradePointAverage(grades.getMathGradeResults())).thenReturn(85.32);
 		
-		verify(repo,times(1)).addGradeResultsForSingleClass(students.getStudentGrades().getMathGradeResults());
-		
-		
+		assertEquals(85.32, service.findGradePointAverage(students.getStudentGrades().getMathGradeResults()));
 	}
 }
