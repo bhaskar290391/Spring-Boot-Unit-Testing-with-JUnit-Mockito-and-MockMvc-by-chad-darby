77924eae1333d1a7fcfa217d5d265f113609c71c
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-03-16 09:39:20 +0530

    adding script using sql

--- Bhaskar_code/04-SpringMVC-UnitTestingAndIntegrationTesting/src/test/java/com/luv2code/springmvc/StudentGradeServiceTest.java
+++ Bhaskar_code/04-SpringMVC-UnitTestingAndIntegrationTesting/src/test/java/com/luv2code/springmvc/StudentGradeServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.luv2code.springmvc.dao.StudentDao;
 import com.luv2code.springmvc.models.CollegeStudent;
@@ -66,6 +67,7 @@ public class StudentGradeServiceTest {
 		assertFalse(deletedStudent.isPresent());
 	}
 	
+	@Sql("/insert-data.sql")
 	@Test
 	public void checkNumberOfStudents() {
 		
@@ -79,7 +81,7 @@ public class StudentGradeServiceTest {
 			
 		}
 		
-		assertEquals(1, data.size());
+		assertEquals(5, data.size());
 	}
 
 	@AfterEach
---
+++ Bhaskar_code/04-SpringMVC-UnitTestingAndIntegrationTesting/target/classes/insert-data.sql
@@ -0,0 +1,4 @@
+insert into student(id,firstname,lastname,email_address) values (11,'bhaskar','mudaliyar','kanishk@gmail,com');
+insert into student(id,firstname,lastname,email_address) values (12,'bhaskar','mudaliyar','kanishk@gmail,com');
+insert into student(id,firstname,lastname,email_address) values (13,'bhaskar','mudaliyar','kanishk@gmail,com');
+insert into student(id,firstname,lastname,email_address) values (14,'bhaskar','mudaliyar','kanishk@gmail,com');
